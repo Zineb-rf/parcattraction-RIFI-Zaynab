cfd8391dfd9e9d5b7dfb53011af0cc474e2ca0df
Zaynab RIFI <zaynab.rifi4@etu.univ-poitiers.fr>
2026-02-10 13:50:25 +0100

    feat(frontend): Amélioration du composant Accueil avec affichage des critiques

--- parc/src/app/accueil/accueil.component.ts
+++ parc/src/app/accueil/accueil.component.ts
@@ -1,21 +1,32 @@
 import { Component } from '@angular/core';
-import { AttractionService } from '../Service/attraction.service';
 import { CommonModule } from '@angular/common';
 import { Observable } from 'rxjs';
+
+import { AttractionService } from '../Service/attraction.service';
 import { AttractionInterface } from '../Interface/attraction.interface';
+
+// Angular Material
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-accueil',
   standalone: true,
-  imports: [CommonModule, MatCardModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './accueil.component.html',
   styleUrl: './accueil.component.scss'
 })
 export class AccueilComponent {
 
-  constructor(public attractionService: AttractionService)
-  {}
-  
-  public attractions: Observable<AttractionInterface[]> = this.attractionService.getAllAttraction()
+  public attractions: Observable<AttractionInterface[]>;
+
+  constructor(public attractionService: AttractionService) {
+    this.attractions = this.attractionService.getAllAttraction();
+  }
 }
