39a09cfada300dbf38309e3524168f887361768c
Zaynab RIFI <zaynab.rifi4@etu.univ-poitiers.fr>
2026-02-10 13:54:46 +0100

    fix(frontend): Modification du script init pour utiliser npm install

--- parc/init-angular.sh
+++ parc/init-angular.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env sh
-
 echo "Installation des packages NPM"
-# installation si packages manquant
-
-npm ls --porcelain > /dev/null || (npm clean-install --legacy-peer-deps)
+# Installation avec npm install au lieu de npm ci
+npm install --legacy-peer-deps
 echo "Done..."
-
 echo "Angular initialisé..."
 
 npm run serve
